3e23f8e0a5e867b1a2be31f8a74714dfebb0839f
K5desu <dekinousyonn@gmail.com>
2025-03-02 00:46:59 +0900

    動くように直した

--- index.js
+++ index.js
@@ -1,23 +1,17 @@
-// {}の必要性がよくわからない。名前つきimport({}がついてる)とデフォルトのimportの違い?
-import { currentServer } from "http";
+import { createServer } from "http";
 import { Server } from "socket.io";
 import express from "express";
 
-//定数を入れ子構造にしてる、app->http->io
 const app = express();
-const http = currentServer(app);
+const http = createServer(app);
 const io = new Server(http);
++(+(
+  // 静的ファイルを配置するディレクトリを指定
+  (+app.use(express.static("public")))
+));
 
-// ディレクトリを指定
-app.use(express.static("public"));
-
-// これ何してるかわからない。
-// io.onとは？ 
-io.on("connection", (socket) => 
-    // onAny = 何でも？
-    // emit = 排出
-    // そもそも => がわからん。
-    socket.onAny((event, data) => socket.broadcast.emit(event, data)),
+io.on("connection", (socket) =>
+  socket.onAny((event, data) => socket.broadcast.emit(event, data))
 );
 
 http.listen(Number(process.env.PORT) || 3000);
--- public/index.js
+++ public/index.js
@@ -1,65 +1,82 @@
+// Socket.ioのフロントエンド・モジュールはバックエンドからインポート可能
 import "/socket.io/socket.io.js";
 
 const pc = new RTCPeerConnection({
-    iceServers: [{ url: ["stun:stun.l.google.com:19302"] }],
+  // GoogleのSTUNサーバーを指定
+  iceServers: [{ urls: ["stun:stun.l.google.com:19302"] }],
 });
 const socket = io();
 
 globalThis.onClickBtn = async () => {
-    const stream = await navigator.mediaDevices.getUserMedia({
-        audio: true,
-        video: true,
-    });
-    for (const track of stream.getTracks()) {
-        pc.addTrack(track);
-    }
-    const video = document.createElement("video")
+  // 端末のカメラとマイクのアクセスをリクエスト
+  const stream = await navigator.mediaDevices.getUserMedia({
+    audio: true,
+    video: true,
+  });
+  for (const track of stream.getTracks()) {
+    pc.addTrack(track);
+  }
+  const video = document.createElement("video");
 
-    video.playsInline = true;
-    video.muted = true;
+  // ブラウザのポリシーによる映像再生エラーの回避
+  video.playsInline = true;
+  video.muted = true;
 
-    video.style.width = "100%";
-    video.srcObject = stream;
-    video.play();
-    document.body.appendChild(video);
+  video.style.width = "100%";
+  // MediaStreamをvideoタグにアタッチ
+  video.srcObject = stream;
+  video.play();
+  document.body.appendChild(video);
 
-    pc.createOffer().then((desc) => {
-        pc.setLocalDescription(desc);
-        socekt.emit("offer", desc);
-    });
+  // LocalDescriptionを生成
+  pc.createOffer().then((desc) => {
+    // LocalDescriptionをPeerConnectionにセット
+    pc.setLocalDescription(desc);
+    // LocalDescriptionをリモートユーザーへ送信
+    socket.emit("offer", desc);
+  });
 };
 
+// リモートユーザーがPeerConnectionにMediaStreamTrackを追加したら発火
 pc.addEventListener("track", ({ track }) => {
-    if (track.kind === "video") {
-        const video = document.createElement("video");
-        video.playsInline = true;
-        video.muted = true;
-        video.width = "100%";
-        video.srcObject = new MediaStream([track]);
-        video.play();
-        document.body.appendChild(video);
-    }
-    if (track.kind === "audio") {
-        const audio = document.createElement("audio")
-        audio.srcObject = new MediaStream([track]);
-        audio.play();
-    }
+  if (track.kind === "video") {
+    const video = document.createElement("video");
+    video.playsInline = true;
+    video.muted = true;
+    video.style.width = "100%";
+    video.srcObject = new MediaStream([track]);
+    video.play();
+    document.body.appendChild(video);
+  }
+  if (track.kind === "audio") {
+    const audio = document.createElement("audio");
+    audio.srcObject = new MediaStream([track]);
+    audio.play();
+  }
 });
-
+// RTCPeerConnection.setLocalDescription()の呼び出しに応じて、
+// ICE Candidateが見つかった時や収集が終了した際に発火
 pc.addEventListener("icecandidate", ({ candidate }) => {
-    if (candidate) {
-        socket.emit("ice", candidate);
-    }
+  if (candidate) {
+    // ICE Candidateをリモートユーザーへ送信
+    socket.emit("ice", candidate);
+  }
 });
 
 socket
-    .on("offer", (desc) => {
-        pc.setRemoteDescription(desc);
-        pc.createAnswer().then((desc) => {
-            pc.setLocalDescription(desc);
-            socket.emit("answer", desc);
-        });
-    })
-
-    .on("answer", (desc) => pc.setRemoteDescription(desc))
-    .on("ice", (candidate) => pc.addIceCandidate(candidate));
+  // リモートユーザーのofferイベントの受信
+  .on("offer", (desc) => {
+    // RemoteDescriptionをPeerConnectionにセット
+    pc.setRemoteDescription(desc);
+    // LocalDescriptionを生成
+    pc.createAnswer().then((desc) => {
+      // LocalDescriptionをPeerConnectionにセット
+      pc.setLocalDescription(desc);
+      // LocalDescriptionをリモートユーザーへ送信
+      socket.emit("answer", desc);
+    });
+  })
+  // リモートユーザーのanswerイベントを受信し、RemoteDescriptionをPeerConnectionにセット
+  .on("answer", (desc) => pc.setRemoteDescription(desc))
+  // リモートユーザーのiceイベントを受信し、ICE Candidateを追加
+  .on("ice", (candidate) => pc.addIceCandidate(candidate));
